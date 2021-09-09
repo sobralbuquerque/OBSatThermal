81a3e0e1b3dadda83fa08f668583e8ce0c7f9189
guilherme.sobral <guilherme.sobral@lnls.br>
2021-09-09 17:53:08 -0300

    correct system input

--- solve.jl
+++ solve.jl
@@ -2,21 +2,21 @@ include("thermalsystem.jl")
 using UnitfulRecipes, Plots, DifferentialEquations
 plotly()
 
-begin
-    T0 = 25.0u"°C" |> u"K"
-    k_iso = 0.032u"W/m/K"
-    L_iso = 50.0u"mm"
-    R_wall = L_iso / k_iso
-    Cp_battery = 800 * 45.8e-3u"J/K"
-    L_battery = [9.2, 21.5, 27.3]u"mm"
-    Q_battery = 50.0u"mW"
-    T_ext = -50.0u"°C" |> u"K"
 
-end
+T0 = 25.0u"°C" |> u"K"
+k_iso = 0.032u"W/m/K"
+L_iso = 50.0u"mm"
+R_wall = L_iso / k_iso
+Cp_battery = 800 * 45.8e-3u"J/K"
+L_battery = [9.2, 21.5, 27.3]u"mm"
+Q_battery = 50.0u"mW"
+T_ext = -50.0u"°C" |> u"K"
+
+
 
 function f(T, p, t)
     L_battery, Q_battery = p
-    thermal_system(T, T_air, P_air, R_wall, Cp_battery, L_battery, Q_battery)
+    thermal_system(T, T_ext, P_air, R_wall, Cp_battery, L_battery, Q_battery)
 end
 p = (L_battery, Q_battery)
 
@@ -25,4 +25,4 @@ tspan = (t[begin], t[end])
 
 prob = ODEProblem(f, T0, tspan, p)
 sol = solve(prob, Tsit5(), saveat = t)
-plot(sol.t, sol.u, yunit = u"°C")
+plot(sol.t, sol.u, yunit = u"°C", xlabel="Time", ylabel="Temperature")
