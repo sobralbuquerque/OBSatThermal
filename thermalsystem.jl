484b541d69edf62ca88ba62bca5b43d72cc8c863
guilherme.sobral <guilherme.sobral@lnls.br>
2021-09-14 10:47:35 -0300

    add Interface struct

--- thermalsystem.jl
+++ thermalsystem.jl
@@ -1,7 +1,16 @@
+import CoolProp
 using StaticArrays, Unitful
+using Unitful: σ
 
+const ε = 0.8
 const g = 9.81u"m/s^2"
 
+air_prop(property_name, T, P) = CoolProp.PropsSI(property_name, "T", T, "P", P, "Air")
+air_density(T, P = P_air) = air_prop("Dmass", T, P)
+air_viscosity(T, P = P_air) = air_prop("viscosity", T, P)
+air_viscosity(T::Quantity, P = P_air) = air_prop("viscosity", T, P) * u"Pa*s"
+air_conductivity(T, P = P_air) = air_prop("conductivity", T, P)
+air_prandtl(T, P = P_air) = air_prop("Prandtl", T, P)
 
 mutable struct Air
     T::Any
@@ -18,6 +27,57 @@ density(air::Air) = air_density(air.T, air.P)
 viscosity(air::Air) = air_viscosity(air.T, air.P)
 conductivity(air::Air) = air_conductivity(air.T, air.P)
 prandtl(air::Air) = air_prandtl(air.T, air.P)
+mutable struct Wall
+    T::Any
+    L::Any
+    R::Any
+end
+
+struct Interface
+    air::Air
+    wall::Wall
+end
+
+density(i::Interface) = air_density(i.T, i.P)
+viscosity(i::Interface) = air_viscosity(i.T, i.P)
+conductivity(i::Interface) = air_conductivity(i.T, i.P)
+prandtl(i::Interface) = air_prandtl(i.T, i.P)
+
+function Base.getproperty(i::Interface, sym::Symbol)
+    if sym === :T
+        # Average temperature
+        (i.air.T + i.wall.T) / 2
+    elseif sym === :ΔT
+        # Temperature gradient
+        abs(i.air.T - i.wall.T)
+    elseif sym === :P
+        # Pressure
+        i.air.P
+    elseif sym === :k
+        conductivity(i)
+    elseif sym === :ρ
+        density(i)
+    elseif sym === :μ
+        viscosity(i)
+    elseif sym === :Pr
+        prandtl(i)
+    elseif sym === :v
+        # Dynamic viscosity
+        i.μ / i.ρ
+    elseif sym === :α
+        # Thermal diffusivity
+        i.v / i.Pr
+    elseif sym === :β
+        1 / i.air.T
+    elseif sym === :Ra
+        # Rayleigh number
+        local Ra = g * i.β * i.ΔT * i.wall.L^3 / (i.α * i.v)
+        NoUnits(Ra)
+    else
+        getfield(i, sym)
+    end
+end
+
 
 
 function pressure(h, T)
@@ -26,7 +86,6 @@ function pressure(h, T)
     R = Unitful.R
     P₀ * exp(-g * M * h / (R * T))
 end
-
 
 
 struct Material
@@ -58,35 +117,24 @@ battery = Cuboid(SVector(1, 2, 3)u"mm", 10u"mm", LI_PO)
 resistence(c::Cuboid) = c.t / c.material.k
 
 
-function parameters(air::Air, T_wall, L)
-    dT = abs(air.T - T_wall)
-    ρ = density(air)
-    μ = viscosity(air)
-    Pr = prandtl(air)
-    k = conductivity(air)
-    v = μ / ρ # dynamic viscosity
-    α = v / Pr # thermal diffusivity
-    β = 1 / air.T
-    Ra = (g * β * dT * L^3) / (α * v)
-    (k, Pr, NoUnits(Ra))
-end
 
-function setvalues!(air::Air, data, t)
-    air.T = data.temperature(t)
-    air.u = data.velocity(t)
-    h = data.altitude(t)
+function setvalues!(air::Air, t)
+    air.T = temperature(t)
+    air.u = velocity(t)
+    h = altitude(t)
     air.P = pressure(h, air.T)
 end
 
-function forced_convection(air::Air, T_wall, L)
-    k, Pr, Ra = parameters(air, T_wall, L)
+function forced_convection(i::Interface, L)
+    k, Pr = i.k, i.Pr
+    u, v = i.air.u, i.air.v
 
     Re_laminar = 4000
     Re_transient = 8.7e5
 
-    Re_L = (L * air.u) / sym
+    Re_L = (L * u) / v
 
-    if Re_L < Re_transient && Pre > 0.6
+    if Re_L < Re_transient && Pr > 0.6
         Nu = 0.664 * √Re_L * ∛Pr
     else
         c = 0.992log10(Re_laminar)
@@ -100,19 +148,19 @@ function forced_convection(air::Air, T_wall, L)
 end
 
 
-function convective_vertical(air::Air, T_wall, L)
-    k, Pr, Ra = parameters(air, T_wall, L)
+function convective_vertical(i::Interface, L)
+    k, Pr, Ra = i.k, i.Pr, i.Ra
 
     Nu = 0.68 + 0.67 * (Ra^0.25) * (1 + (0.492 / Pr)^(9 / 16)^(-4 / 9))
 
     (k * Nu) / L
 end
 
-function convective_horizontal(air::T, T_wall, L, A, P)
-    k, Pr, Ra = parameters(air, T_wall, L)
+function convective_horizontal(i::Interface, L, A, P)
+    k, Pr, Ra = i.k, i.Pr, i.Ra
 
     if (0.24 < Pr < 2000) && Ra < 1e7
-        Nu = 0.14 * Ra^(1 / 3) * (1 + 0.0107Pr) / (1 + 0.01Pr)
+        Nu = 0.14Ra^(1 / 3) * (1 + 0.0107Pr) / (1 + 0.01Pr)
     else
         Ra = rayleigh_air(T_med, dT, A / P)
         Nu = (0.56Ra^0.25) / (1 + (0.492 / Pr)^(9 / 16))^(4 / 9)
@@ -123,15 +171,20 @@ end
 
 R_equiv(R, h_rad, h) = R + 1 / h_rad + 1 / h
 
-function UA_battery(air::Air, T_wall, R_wall, h_rad, L1, L2, L3)
+function UA_battery(i::Interface, h_rad, L1, L2, L3)
     A1 = L2 * L3
     A2 = L1 * L2
     A3 = L1 * L3
     At = 2(A1 + A2 + A3)
     P2 = 2(L2 + L3)
 
-    h_vertical = convective_vertical(air, T_wall, L1)
-    h_horizontal = convective_horizontal(air, T_wall, L2, A2, P2)
+    if abs(i.air.u) > 0
+        h_vertical = forced_convection(i, L1)
+    else
+        h_vertical = convective_vertical(i, L1)
+    end
+
+    h_horizontal = convective_horizontal(i, L2, A2, P2)
 
     R_h = R_equiv(R_wall, h_rad, h_horizontal)
     R_v1 = R_equiv(R_wall, h_rad, h_vertical)
@@ -141,18 +194,18 @@ function UA_battery(air::Air, T_wall, R_wall, h_rad, L1, L2, L3)
     At * G_eq
 end
 
-function wall_temperatures(air::Air, T_wall, T_battery, R_wall, h_rad, L1, L2, L3)
+function wall_temperatures(i, T_battery, h_rad, L1, L2, L3)
     A1 = L2 * L3
     A2 = L1 * L2
     A3 = L1 * L3
     At = 2(A1 + A2 + A3)
     P2 = 2(L2 + L3)
 
-    UA = UA_battery(air, T_wall, R_wall, h_rad, L1, L2, L3)
+    UA = UA_battery(i, h_rad, L1, L2, L3)
     q = (T_battery - T_air) * UA
 
-    h_vertical = convective_vertical(air, T_wall, L1)
-    h_horizontal = convective_horizontal(air, T_wall, L2, A2, P2)
+    h_vertical = convective_vertical(i, L1)
+    h_horizontal = convective_horizontal(i, L2, A2, P2)
     R_h = R_wall + 1 / h_horizontal
     R_v1 = R_wall + 1 / h_vertical
     R_v2 = R_wall + 1 / h_vertical
@@ -173,16 +226,6 @@ L_battery = [9.2, 21.5, 27.3]u"mm"
 Q_battery = 50.0u"mW"
 T_ext = -50.0u"°C" |> u"K"
 air = Air(T_ext)
-
-convective_vertical(air, T0, L_iso)
-
-
-using Symbolics
-
-begin
-    @syms L1 L2 L3
-    A1 = L2 * L3
-    A2 = L1 * L2
-    A3 = L1 * L3
-    At = 2(A1 + A2 + A3)
-end
+wall = Wall(T_ext, L_iso, R_wall)
+interface = Interface(air, wall)
+convective_vertical(interface, L_iso)
